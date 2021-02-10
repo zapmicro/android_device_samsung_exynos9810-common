Roynas-Android-Playground/device_samsung_universal9611-common_old

hardware: create vulkan/opencl symlinks to libGLES_mali.so
Change-Id: Ie26232f9599e1a3f97ccc3e3ee6d8600ce03ba64

hardware/gles/vulkan.mk
@@ -1,0 +1,39 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
+LIBGLES_MALI64_LIBRARY := /vendor/lib64/egl/libGLES_mali.so
+
+VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/vulkan.universal9611.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/hw/vulkan.universal9611.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
+
+VULKAN64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.universal9611.so
+$(VULKAN64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/hw/vulkan.universal9611.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI64_LIBRARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(VULKAN_SYMLINK) \
+	$(VULKAN64_SYMLINK)

zapmicro/android_device_samsung_exynos9810-common

hardware/gles/vulkan.mk
@@ -1,0 +1,39 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
+LIBGLES_MALI64_LIBRARY := /vendor/lib64/egl/libGLES_mali.so
+
+VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/vulkan.universal9810.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/hw/vulkan.universal9611.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
+
+VULKAN64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.universal9810.so
+$(VULKAN64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/hw/vulkan.universal9611.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI64_LIBRARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(VULKAN_SYMLINK) \
+	$(VULKAN64_SYMLINK)